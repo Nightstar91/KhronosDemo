cf8595068466140bb9fcef1b976af68d6646e330
ALVARO\alvar <alvarotron91@gmail.com>
2025-10-10 21:09:46 -0500

    - Made camera adjustment, updated player prefab and added camera shake (however code refactor is required)

--- Assets/Alvaro Work/Scripts/CameraEffect.cs
+++ Assets/Alvaro Work/Scripts/CameraEffect.cs
@@ -1,21 +1,28 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class CameraEffect : MonoBehaviour
 {
     [Header("Camera Sway Parameters")]
-    [Range(0.001f, 0.01f)] public float amount = 0.1f;
+    [Range(0, 0.1f)] public float amount = 0.01f;
     [Range(1f, 30f)] public float frequency = 15.0f;
     [Range(10f, 100f)] public float smooth = 10.0f;
 
     [Header("Camera Tilt Parameters")]
-    [Range(0f, 0.5f)] public float tiltAmount = 0.5f;
+    [Range(0f, 0.5f)] public float tiltAmount = 0.1f;
     [Range(0f, 1f)] public float tiltSpeed = 0.4f;
     [Range(0f, 3f)] public float tiltResetSpeed = 1.5f;
 
     [Header("Camera Shake Parameters")]
     [Range(0f, 1f)] public float shakeDuration = 0.5f;
+    public AnimationCurve curve;
     [Range(10f, 100f)] public float shakeIntensity;
+
+    [SerializeField] private Transform _camera;
+    [SerializeField] private Transform _cameraHolder;
+
+    public bool start = false;
 
     Vector3 startPosition;
     Quaternion startRotation;
@@ -33,14 +40,15 @@ public class CameraEffect : MonoBehaviour
     void Update()
     {
         CheckForInput();
+        _camera.LookAt(FocusTarget());
         StopHeadbob();
         StopSwayCamera();
 
-         if (FPSController.jumpAction.IsPressed() && !FPSController.isGrounded)
+        if (!FPSController.isGrounded)
         {
-            if (FPSController.CheckIfGrounded())
+            if(FPSController.isGrounded)
             {
-                StartCameraShake();
+                StartCoroutine(CameraShakeEvent());
             }
         }
     }
@@ -52,36 +60,45 @@ public class CameraEffect : MonoBehaviour
         float horizontalAxis = Input.GetAxis("Horizontal");
         StartSwayCamera(horizontalAxis);
 
-        if (inputMagnitide > 0)
+        if (inputMagnitide > 0 && FPSController.isGrounded)
         {
             StartHeadbob();
         }
     }
 
-
-
     
     private Vector3 StartHeadbob()
     {
         Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Lerp(pos.y, Mathf.Sin(Time.time * frequency) * amount * 1.4f, smooth * Time.deltaTime);
-        pos.x += Mathf.Lerp(pos.x, Mathf.Cos(Time.time * frequency) * amount * 1.4f, smooth * Time.deltaTime);
+        pos.y += Mathf.Sin(Time.time * frequency) * amount;
+        pos.x += Mathf.Cos(Time.time * frequency / 3) * amount;
 
         transform.localPosition += pos;
 
         return pos;
     }
 
-
-    private Vector3 StartCameraShake()
+    // https://www.youtube.com/watch?v=BQGTdRhGmE4
+    IEnumerator CameraShakeEvent()
     {
-        Vector3 pos = Vector3.zero;
+        float elapsedTime = 0f;
 
-        pos.y += Mathf.Lerp(pos.y, Mathf.Sin(Time.time * frequency) * shakeIntensity * 1.4f, shakeDuration * Time.deltaTime);
-        pos.x += Mathf.Lerp(pos.y, Mathf.Sin(Time.time * frequency) * shakeIntensity * 1.4f, shakeDuration * Time.deltaTime);
+        while (elapsedTime < shakeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / shakeDuration);
+            transform.localPosition = Random.insideUnitSphere * strength;
+            yield return null;
+        }
 
-        transform.localPosition += pos;
+        transform.position = startPosition;
+    }
 
+
+    private Vector3 FocusTarget()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
+        pos += _cameraHolder.forward * 15.0f;
         return pos;
     }
 
@@ -89,7 +106,7 @@ public class CameraEffect : MonoBehaviour
     private void StopHeadbob()
     {
         if (transform.localPosition == startPosition) return;
-        transform.localPosition = Vector3.Lerp(transform.localPosition, startPosition, 1 * Time.deltaTime);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, startPosition, smooth * Time.deltaTime);
     }
 
 
