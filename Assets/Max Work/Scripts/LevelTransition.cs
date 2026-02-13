15883cfa901c4eaa1960e13aaef3a6028c308f72
DracoAndTorch <dracoandtorch@gmail.com>
2026-02-13 16:30:31 -0600

    Populated Work Folder and Made Scene Transition

---
+++ Assets/Max Work/Scripts/LevelTransition.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string[] allScenes;
+    Scene currentScene;
+    int Scene;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        Scene = FindScene();
+
+        if(Scene == -1)
+        {
+            Debug.Log("SCENE NOT LOADED IN ARRAY!!!!!");
+        }
+    }
+
+    private int FindScene()
+    {
+        int thisScene = -1;
+        for (int i = 0; i < allScenes.Length - 1; i++)
+        {
+            if (allScenes[i] == currentScene.name)
+            {
+                thisScene = i;
+            }
+        }
+        return thisScene;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Player" || Scene == -1)
+        {
+            return;
+        }
+
+        if(Scene == allScenes.Length - 1)
+        {
+            SceneManager.LoadScene(allScenes[0]);
+        }
+        else
+        {
+            SceneManager.LoadScene(allScenes[Scene + 1]);
+        }
+    }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(allScenes[0]);
+    }
+}
