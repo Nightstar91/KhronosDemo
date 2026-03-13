f045efcd120413735025fabefcf5825e54f82f0e
ALVARO\alvar <alvarotron91@gmail.com>
2026-03-13 10:16:08 -0500

    - Moving coin testing to personal scene, starting on gate objective

--- Assets/Alvaro Work/Scripts/Level Scripting/Coin.cs
+++ Assets/Alvaro Work/Scripts/Level Scripting/Coin.cs
@@ -4,16 +4,20 @@ public class Coin : MonoBehaviour
 {
     [Header("This is to link to the door that will unlock once all the coins have been collected")]
     [SerializeField] string doorToUnlock;
+    private bool hasBeenCollected = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player") return;
 
         gameObject.SetActive(false);
+        hasBeenCollected = true;
+
     }
 
     private void ResetCoin()
     {
         gameObject.SetActive(true);
+        hasBeenCollected = false;
     }
 }
