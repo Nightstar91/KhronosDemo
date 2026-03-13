6ad30eb7e91d85873e6409dfaf753b00ea2350b7
ALVARO\alvar <alvarotron91@gmail.com>
2026-03-13 11:54:27 -0500

    -final commit before class

--- Assets/Alvaro Work/Scripts/Level Scripting/Coin.cs
+++ Assets/Alvaro Work/Scripts/Level Scripting/Coin.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [Header("This is to link to the door that will unlock once all the coins have been collected")]
-    [SerializeField] string doorToUnlock;
+    [SerializeField] public string name;
     private bool hasBeenCollected = false;
 
     private void OnTriggerEnter(Collider other)
