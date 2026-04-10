4569ce6382c1266b8cab47107e9fc58da3946a87
DracoAndTorch <dracoandtorch@gmail.com>
2026-04-10 14:04:35 -0500

    Add Checkpoint

---
+++ Assets/CheckpointBarrier.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class CheckpointBarrier : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+    }
+}
