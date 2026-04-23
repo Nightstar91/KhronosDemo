211d5e58e0268b76e88c6d3535d2f4a448c4de64
ALVARO\alvar <alvarotron91@gmail.com>
2026-04-23 12:25:17 -0500

    -HUSH HUSH LEADERBOARD STUFF

--- Assets/Alvaro Work/Scripts/Leaderboard.cs
+++ Assets/Alvaro Work/Scripts/Leaderboard.cs
@@ -5,15 +5,16 @@ using UnityEngine.Rendering;
 
 public class Leaderboard : MonoBehaviour
 {
-    TimeSpan time = TimeSpan.Zero;
 
-    // 0 being the fastest
-    public float[] leaderboardTime = new float[4];
-    public string[] leaderboardName = new string[3];
+    // Setup for struct
+    private const int TOTAL_ENTRIES = 4;
+    private const int TOTAL_LAPS = 4;
 
-    public float[] firstPlaceLapTime = new float[4];
-    public float[] secondPlaceLapTime = new float[4];
-    public float[] thirdPlaceLapTime = new float[4];
+    string[] placePrefix = { "1st", "2nd", "3rd", "user" };
+    string[] lapNames = { "First", "Second", "Third", "Fourth" };
+
+    public LeaderboardEntry[] leaderboard = new LeaderboardEntry[TOTAL_ENTRIES];
+
 
     // Related to the player score/time
     public float currentTime;
@@ -42,6 +43,14 @@ public class Leaderboard : MonoBehaviour
     [SerializeField] TextMeshProUGUI thirdPlaceLapFourTimeText;
     [SerializeField] TextMeshProUGUI thirdPlaceNameText;
 
+    [System.Serializable]
+    public struct LeaderboardEntry
+    {
+        public string name;
+        public float totalTime;
+        public float[] lapTimes; // length = number of laps
+    }
+
     private void Start()
     {
         LoadLeaderBoard();
@@ -51,91 +60,69 @@ public class Leaderboard : MonoBehaviour
 
     public void LoadLeaderBoard()
     {
-        // First place position
-        leaderboardTime[0] = PlayerPrefs.GetFloat("FirstPlaceTimer", 240);
-        leaderboardName[0] = PlayerPrefs.GetString("FirstPlaceName", "Dev1");
+        for (int i = 0; i < TOTAL_ENTRIES; i++)
+        {
+            leaderboard[i].name = PlayerPrefs.GetString($"{placePrefix[i]}PlaceName", $"Dev{i + 1}");
+            leaderboard[i].totalTime = PlayerPrefs.GetFloat($"{placePrefix[i]}PlaceTimer", 0f);
+            leaderboard[i].lapTimes = new float[TOTAL_LAPS];
 
-        firstPlaceLapTime[0] = PlayerPrefs.GetFloat("1stFirstLapTime", 35);
-        firstPlaceLapTime[1] = PlayerPrefs.GetFloat("1stSecondLapTime", 45);
-        firstPlaceLapTime[2] = PlayerPrefs.GetFloat("1stThirdLapTime", 65);
-        firstPlaceLapTime[3] = PlayerPrefs.GetFloat("1stFourthLapTime", 95);
-
-        // Second place
-        leaderboardTime[1] = PlayerPrefs.GetFloat("SecondPlaceTimer", 420);
-        leaderboardName[1] = PlayerPrefs.GetString("SecondPlaceName", "Dev2");
-
-        secondPlaceLapTime[0] = PlayerPrefs.GetFloat("2ndFirstLapTime", 65);
-        secondPlaceLapTime[1] = PlayerPrefs.GetFloat("2ndSecondLapTime", 85);
-        secondPlaceLapTime[2] = PlayerPrefs.GetFloat("2ndThirdLapTime", 105);
-        secondPlaceLapTime[3] = PlayerPrefs.GetFloat("2ndFourthLapTime", 160);
-
-        // Third place
-        leaderboardTime[2] = PlayerPrefs.GetFloat("ThirdPlaceTimer", 600);
-        leaderboardName[2] = PlayerPrefs.GetString("ThirdPlaceName", "Dev3");
-
-        thirdPlaceLapTime[0] = PlayerPrefs.GetFloat("3rdFirstLapTime", 130);
-        thirdPlaceLapTime[1] = PlayerPrefs.GetFloat("3rdSecondLapTime", 180);
-        thirdPlaceLapTime[2] = PlayerPrefs.GetFloat("3rdThirdLapTime", 290);
-        thirdPlaceLapTime[3] = PlayerPrefs.GetFloat("3rdFourthLapTime", 400);
-
-        //Debug.Log($"Loaded: \n{leaderboardName[0]}: {leaderboardTime[0]}\n1st Lap:{firstPlaceLapTime[0]}  2nd Lap:{firstPlaceLapTime[1]}  3rd Lap:{firstPlaceLapTime[2]}  4th Lap:{firstPlaceLapTime[3]}, \n\n {leaderboardTime[1]}: {leaderboardName[1]}, \n {leaderboardTime[2]}: {leaderboardName[2]}");
+            for (int lap = 0; lap < TOTAL_LAPS; lap++)
+            {
+                string key = $"{placePrefix[i]}{lapNames[lap]}LapTime";
+                leaderboard[i].lapTimes[lap] = PlayerPrefs.GetFloat(key, 0f);
+            }
+        }
     }
+
+
+    public void Bubblesort(int numberOfEntries)
+    {
+        for (int i = 0; i < numberOfEntries - 1; i++)
+        {
+            for (int j = 0; j < numberOfEntries - 1 - i; j++)
+            {
+                if (leaderboard[j].totalTime > leaderboard[j + 1].totalTime)
+                {
+                    LeaderboardEntry temp = leaderboard[j];
+                    leaderboard[j]        = leaderboard[j + 1];
+                    leaderboard[j + 1]    = temp;
+                }
+            }
+        }
+    }
+
 
     public void DisplayCurrentLeaderboard()
     {
-        // 1st place
-        firstPlaceTimeText.text = string.Format(ConvertFloatToTime(leaderboardTime[0]));
-        firstPlaceLapOneTimeText.text = ConvertFloatToTime(firstPlaceLapTime[0]);
-        firstPlaceLapTwoTimeText.text = ConvertFloatToTime(firstPlaceLapTime[1]);
-        firstPlaceLapThreeTimeText.text = ConvertFloatToTime(firstPlaceLapTime[2]);
-        firstPlaceLapFourTimeText.text = ConvertFloatToTime(firstPlaceLapTime[3]);
-        firstPlaceNameText.text = leaderboardName[0];
+        //// 1st place
+        //firstPlaceTimeText.text = 
+        //firstPlaceLapOneTimeText.text = 
+        //firstPlaceLapTwoTimeText.text = 
+        //firstPlaceLapThreeTimeText.text = 
+        //firstPlaceLapFourTimeText.text = 
+        //firstPlaceNameText.text = 
 
-        // 2nd place
-        secondPlaceTimeText.text = ConvertFloatToTime(leaderboardTime[1]);
-        secondPlaceLapOneTimeText.text = ConvertFloatToTime(secondPlaceLapTime[0]);
-        secondPlaceLapTwoTimeText.text = ConvertFloatToTime(secondPlaceLapTime[1]);
-        secondPlaceLapThreeTimeText.text = ConvertFloatToTime(secondPlaceLapTime[2]);
-        secondPlaceLapFourTimeText.text = ConvertFloatToTime(secondPlaceLapTime[3]);
-        secondPlaceNameText.text = leaderboardName[1];
+        //// 2nd place
+        //secondPlaceTimeText.text = 
+        //secondPlaceLapOneTimeText.text = 
+        //secondPlaceLapTwoTimeText.text = 
+        //secondPlaceLapThreeTimeText.text = 
+        //secondPlaceLapFourTimeText.text = 
+        //secondPlaceNameText.text = 
 
-        // 3rd place
-        thirdPlaceTimeText.text = ConvertFloatToTime(leaderboardTime[2]);
-        thirdPlaceLapOneTimeText.text = ConvertFloatToTime(thirdPlaceLapTime[0]);
-        thirdPlaceLapTwoTimeText.text = ConvertFloatToTime(thirdPlaceLapTime[1]);
-        thirdPlaceLapThreeTimeText.text = ConvertFloatToTime(thirdPlaceLapTime[2]);
-        thirdPlaceLapFourTimeText.text = ConvertFloatToTime(thirdPlaceLapTime[3]);
-        thirdPlaceNameText.text = leaderboardName[2];
-    }
-
-    private void GetCurrentHighscoreString()
-    {
-        string message;
-        string timeFormatted;
-
-        for (int i = 0; i < leaderboardTime.Length - 1; i++)
-        {
-            timeFormatted = ConvertFloatToTime(leaderboardTime[i]);
-            message = string.Format("{0} : {1:F2}", i + 1, timeFormatted);
-            Debug.Log(message);
-        }
-
-        Debug.Log("END OF LEADERBOARD");
+        //// 3rd place
+        //thirdPlaceTimeText.text = 
+        //thirdPlaceLapOneTimeText.text = 
+        //thirdPlaceLapTwoTimeText.text = 
+        //thirdPlaceLapThreeTimeText.text = 
+        //thirdPlaceLapFourTimeText.text = 
+        //thirdPlaceNameText.text = 
     }
 
 
     public void SetCurrentHighscore()
     {
-        PlayerPrefs.SetFloat("FirstPlaceTimer", leaderboardTime[0]);
-        PlayerPrefs.SetFloat("1stFirstLapTime", firstPlaceLapTime[0]);
-        PlayerPrefs.SetFloat("1stSecondLapTime", firstPlaceLapTime[1]);
-        PlayerPrefs.SetFloat("1stThirdLapTime", firstPlaceLapTime[2]);
-        PlayerPrefs.SetFloat("1stFourthLapTime", firstPlaceLapTime[3]);
 
-        PlayerPrefs.SetFloat("SecondPlaceTimer", leaderboardTime[1]);
-        PlayerPrefs.SetFloat("2ndFirstLapTime", secondPlaceLapTime[0]);
-
-        PlayerPrefs.SetFloat("ThirdPlaceTimer", leaderboardTime[2]);
     }
 
 
@@ -153,74 +140,6 @@ public class Leaderboard : MonoBehaviour
     }
 
 
-    public float GetFirstPlaceTime()
-    {
-        LoadLeaderBoard();
-        Bubblesort();
-        //GetCurrentHighscoreString();
-
-        return leaderboardTime[3];
-    }
-
-
-    private void Bubblesort()
-    {
-        int i, j;
-        float temp;
-        bool swapped;
-        for (i = 0; i < leaderboardTime.Length - 1; i++)
-        {
-            swapped = false;
-            for (j = 0; j < leaderboardTime.Length - i - 1; j++)
-            {
-                if (leaderboardTime[j] > leaderboardTime[j + 1])
-                {
-
-                    // Swap arr[j] and arr[j+1]
-                    temp = leaderboardTime[j];
-                    leaderboardTime[j] = leaderboardTime[j + 1];
-                    leaderboardTime[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-
-            // If no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
-        }
-    }
-
-
-    private void Bubblesort(float newScoreEntry)
-    {
-        leaderboardTime[3] = newScoreEntry;
-
-        int i, j;
-        float temp;
-        bool swapped;
-        for (i = 0; i < leaderboardTime.Length - 1; i++)
-        {
-            swapped = false;
-            for (j = 0; j < leaderboardTime.Length - i - 1; j++)
-            {
-                if (leaderboardTime[j] > leaderboardTime[j + 1])
-                {
-
-                    // Swap arr[j] and arr[j+1]
-                    temp = leaderboardTime[j];
-                    leaderboardTime[j] = leaderboardTime[j + 1];
-                    leaderboardTime[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-
-            // If no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
-        }
-    }
 
 
     private void ResetFloatArray(float[] arrayToReset)
